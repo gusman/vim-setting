20c0adbf1bd9c74de51381ca159f9b1b5ae8f8e6
Gusman DP <gusman.dp@gmail.com>
2011-09-07 14:05:19 +0700

    update ctags genarator

--- gv-project/py-script/Config.py
+++ gv-project/py-script/Config.py
@@ -3,68 +3,102 @@
 import os
 
 class FileConfig:
-	def __init__(self, filename=''): 
-		self.filename = filename
-		self.projectname = ''
-		self.ctagsfile = ''
-		self.cscopefile = ''
+    def __init__(self, filename=''): 
+        self.filename = filename
+        self.projectname = ''
+        self.srcdir = ''
+        self.confdir = ''
+        self.ctagsfile = ''
+        self.cscopefile = ''
 
-	def parse_file(self):
-		tmp = ''
-		rmod = -1
-		self.clear_attr()
+    def parse_file(self):
+        tmp = ''
+        rmod = -1
+        self.clear_attr()
 
-		f = open(self.filename, 'r')
-		while True:
-			c = f.read(1)
-			if (0 == len(c)):
-				break
-			
-			if '#' != c:
-				if (1 == rmod): self.projectname += c
-				elif (2 == rmod): self.ctagsfile += c
-				elif (3 == rmod): self.cscopefile += c
-				elif (0 == rmod): tmp += c
-			else:
-				if ('PRJN' == tmp): rmod = 1
-				elif ('CTAG' == tmp): rmod = 2
-				elif ('CSCP' == tmp): rmod = 3
-				else: rmod = 0
+        f = open(self.filename, 'r')
+        while True:
+            c = f.read(1)
+            if (0 == len(c)):
+                break
 
-				tmp = ''
-		f.close()
+            if '#' != c:
+                if (1 == rmod): self.projectname += c
+                elif (2 == rmod): self.ctagsfile += c
+                elif (3 == rmod): self.cscopefile += c
+                elif (4 == rmod): self.srcdir += c
+                elif (5 == rmod): self.confdir += c
+                elif (0 == rmod): tmp += c
+            else:
+                if ('PRJN' == tmp): rmod = 1
+                elif ('CTAG' == tmp): rmod = 2
+                elif ('CSCP' == tmp): rmod = 3
+                elif ('SRCD' == tmp): rmod = 4
+                elif ('CONF' == tmp): rmod = 5
+                else: rmod = 0
 
-	def dump_tofile(self):
-		f = open(self.filename, 'w')
-		f.write("#PRJN#" + self.projectname + "#\n" \
-				"#CTAG#" + self.ctagsfile + "#\n" \
-				"#CSCP#" + self.cscopefile + "#\n" ) 
+        tmp = ''
+        f.close()
 
-	def set_projectname(self, prj_name):
-		self.projectname = prj_name
+    def dump_tofile(self):
+        f = open(self.filename, 'w')
+        f.write("#PRJN#" + self.projectname + "#\n" \
+                "#CTAG#" + self.ctagsfile + "#\n" \
+                "#CSCP#" + self.cscopefile + "#\n" 
+                "#SRCD#" + self.srcdir + "#\n" \
+                "#CONF#" + self.confdir + "#\n" ) 
 
-	def set_ctagsfile(self, ctags_file):
-		self.ctagsfile = ctags_file
+    def set_filename(self, filename):
+        self.filename = filename
 
-	def set_cscopfile(self, cscope_file):
-		self.cscopefile = cscope_file
+    def set_projectname(self, prj_name):
+        self.projectname = prj_name
 
-	def get_projectname(self):
-		return self.projectname
+    def set_ctagsfile(self, ctags_file):
+        self.ctagsfile = ctags_file
 
-	def get_ctagsfile(self):
-			return self.ctagsfile
+    def set_cscopefile(self, cscope_file):
+        self.cscopefile = cscope_file
 
-	def get_cscopefile(self):
-			return self.cscopefile
+    def set_srcdir(self, srcdir):
+        self.srcdir = srcdir
 
-	def clear_attr(self):
-		self.projectname = ''
-		self.ctagsfile = ''
-		self.cscopefile = ''
-			  
-	def info(self):
-		print "Filename: " + self.filename
-		print "Projectname: " + self.projectname
-		print "Ctagsfile: " + self.ctagsfile
-		print "Cscopefile: " + self.cscopefile
+    def set_confdir(self, confdir):
+        self.confdir = confdir
+
+    def get_filename(self):
+        return self.filename
+
+    def get_projectname(self):
+        return self.projectname
+
+    def get_ctagsfile(self):
+        return self.ctagsfile
+
+    def get_cscopefile(self):
+        return self.cscopefile
+
+    def get_srcdir(self):
+        return self.srcdir
+
+    def get_confdir(self):
+        return self.confdir
+
+
+    def clear_attr(self):
+        self.projectname = ''
+        self.ctagsfile = ''
+        self.cscopefile = ''
+        self.srcdir = ''
+        self.confdir = ''
+
+    def is_fileexist(self):
+        return os.path.isfile(self.filename)
+          
+    def info(self):
+        print "Filename: " + self.filename
+        print "Projectname: " + self.projectname
+        print "Ctagsfile: " + self.ctagsfile
+        print "Cscopefile: " + self.cscopefile
+        print "srcdir: " + self.srcdir
+        print "confdir: " + self.confdir
--- gv-project/py-script/Project.py
+++ gv-project/py-script/Project.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python
 
+import os
 from Config import FileConfig
 
 class Project(FileConfig):
-	def __init__(self, filename):
-		FileConfig.__init__(self, filename)
+    def __init__(self, filename=''):
+        FileConfig.__init__(self, filename)
 
-	def gen_ctags(self):
-		pass
+    def gen_ctags(self):
+        print ">> GENERATE CTAGS FILE"
+        cmd = "ctags -R "  + " -o " + self.get_ctagsfile() + " " + self.get_srcdir()
+        os.system(cmd)
 
-	def gen_csope(self):
-		pass
+    def gen_csope(self):
+        print ">> GENERATE CSCOPE FILE"
+
+myPrj = Project()
+myPrj.set_filename("D:\\git\\vim-setting\\gv-project\\prj-info\\py-script.conf")
+myPrj.set_confdir("D:\\git\\vim-setting\\gv-project\\prj-info")
+myPrj.set_srcdir("D:\\git\\vim-setting\\gv-project\\py-script")
+myPrj.set_ctagsfile(myPrj.get_confdir() + "\\" + "tags")
+myPrj.gen_ctags()
+
