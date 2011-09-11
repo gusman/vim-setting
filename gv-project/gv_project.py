488c11736b11715a9416ba133f00ee8b0c249a4e
unknown <Gusman@.(none)>
2011-09-11 23:09:25 +0700

    simplified python function

---
+++ gv-project/gv_project.py
@@ -0,0 +1,122 @@
+#! /usr/bin/python
+
+import os
+import vim
+
+def dir_trim(str):
+    if '/' != str[len(str) - 1]:
+        str = str + '/'
+    return str
+
+def str_trim(str):
+    if '\n' == str[len(str) - 1]:
+        str = str[:-1]
+    return str
+
+def gv_init():
+    gvdir = vim.eval("g:gv_dir")
+    prjname = vim.eval("g:prj_name")
+
+    if not os.path.isdir(gvdir):
+        os.mkdir(gvdir)
+
+    gvdir = dir_trim(gvdir)
+    confdir = gvdir + prjname
+    if not os.path.isdir(confdir):
+        os.mkdir(confdir)
+
+    confdir = dir_trim(confdir)
+    prj_conf_filepath = confdir + "prj.conf"
+    vim.command("let g:prj_conf_filename = '" + prj_conf_filepath + "'")
+
+    ctagsfile = confdir + "tags"
+    vim.command("let g:ctags_file = '" + ctagsfile + "'")
+
+  
+def gv_dumptofile(filepath, projectname, \
+                ctagsfile, cscopefile, srcdir, confdir ):
+    print ">> DUMP to FILE"
+    f = open(filepath, 'w')
+    f.write("#PRJN#" + projectname + "#\n" \
+            "#CTAG#" + ctagsfile + "#\n" \
+            "#CSCP#" + cscopefile + "#\n" 
+            "#SRCD#" + srcdir + "#\n" \
+            "#CONF#" + confdir + "#\n" ) 
+    f.close()
+
+def gv_parsefile(filepath):
+    print ">> PARSE FILE"
+    projectname = ''
+    ctagsfile = ''
+    cscopefile = ''
+    srcdir = ''
+    confdir = ''
+
+    rmod = -1
+    tmp = ''
+
+    f = open(filepath, 'r')
+    while True:
+        c = f.read(1)
+        if (0 == len(c)):
+            break
+
+        if '#' != c:
+            if 1 == rmod: projectname += c
+            elif 2 == rmod: ctagsfile += c
+            elif 3 == rmod: cscopefile += c
+            elif 4 == rmod: srcdir += c
+            elif 5 == rmod: confdir += c
+            elif 0 == rmod: tmp += c
+        else:
+            if 'PRJN' == tmp: rmod = 1
+            elif 'CTAG' == tmp: rmod = 2
+            elif 'CSCP' == tmp: rmod = 3
+            elif 'SRCD' == tmp: rmod = 4
+            elif 'CONF' == tmp: rmod = 5
+            else: rmod = 0
+            tmp = ''
+
+    f.close()
+    lval = [projectname, ctagsfile, cscopefile, srcdir, confdir]
+    return lval
+
+def gv_getconfig():
+    print ">> GET CONFIG"
+    filepath = vim.eval("g:prj_conf_filename")
+    lval = parse_file(filepath)
+    
+    vim.command("let g:prj_name = '" + lval[0] + "'")
+    vim.command("let g:ctagsfile = '" + lval[1] + "'")
+    vim.command("let g:cscopefile = '" + lval[2] + "'")
+    vim.command("let g:src_dir = '" + lval[3] + "'")
+    vim.command("let g:conf_dir = '" + lval[4] + "'");
+
+def gv_saveconfig():
+    print ">> SAVE CONFIG"
+    filepath = vim.eval("g:prj_conf_filename")
+    projectname = vim.eval("g:prj_name")
+    ctagsfile = vim.eval("g:ctags_file")
+    cscopefile = vim.eval("g:cscope_file")
+    srcdir = vim.eval("g:src_dir")
+    confdir = vim.eval("g:conf_dir")
+
+    dump_tofile(filepath, projectname, ctagsfile, \
+                cscopefile, srcdir, confdir)
+
+def gv_gentags():
+    print ">> GENT TAGS"
+    ctagsfile = vim.eval("g:ctags_file")
+    srcdir = vim.eval("g:src_dir")
+    cmd = "ctags -R "  + " -o " + ctagsfile + " " + srcdir
+    os.system(cmd)
+
+def gv_settags():
+    print ">> SET TAGS"
+    ctagsfile = vim.eval("g:ctags_file")
+    vim.command("set tags = '" + ctagsfile + "'")
+
+def gv_updatetags():
+    print ">> UDPATE TAGS"
+    gv_gentags()
+    gv_settags()
---
+++ gv-project/gv_project.vim
@@ -0,0 +1,33 @@
+" ===========================
+" gv_project global variabel
+" ===========================
+let g:prj_name = 'gv-python'
+let g:ctags_file = ''
+let g:cscope_file = ''
+let g:src_dir = 'D:/git/vim-setting/gv-project'
+let g:gv_dir = $HOME . '/.gvproj'
+let g:conf_dir = ''
+let g:prj_conf_filename = ''
+
+" ==========================
+" Load python file
+" ==========================
+pyfile D:/git/vim-setting/gv-project/project.py
+
+" =========================
+" Function list
+" =========================
+function! GV_ProjectInfo()
+    echo g:prj_name
+    echo g:ctags_file
+    echo g:cscope_file
+    echo g:src_dir
+    echo g:conf_dir
+endfunction
+
+
+" ========================
+" Test area
+" ========================
+py gv_init()
+py gv_gentags()
