b4a271a6d2936e94534140e656c98893e5d237f4
Gusman Dharma Putra <gusman.dp@gmail.com>
2013-12-08 22:23:35 +0700

    update setup for automatically required directory

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 # Install vundle
 PWD=`pwd`
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle && cp $PWD/vimrc ~/.vimrc
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle && ln -sf $PWD/vimrc ~/.vimrc && mkdir -p ~/.vimdata/{swap,backup} 
 
